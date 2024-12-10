d162cefd7e0048073ef0110f87e10940e39ab63b
Mateusz-of-Rivia <mtsmolar@student.agh.edu.pl>
2024-12-10 10:42:39 +0000

    slam ready

--- robo_warehouse/launch/minimal_launch_file.launch.py
+++ robo_warehouse/launch/minimal_launch_file.launch.py
@@ -6,6 +6,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node  # Correct import for Node
 from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
@@ -20,11 +21,32 @@ def generate_launch_description():
     ros2gz_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=['--ros-args', '-p', 'config_file:=/home/developer/ros2_ws/src/robo_warehouse/ros2gz_bridge_config.yaml'],
+        arguments=['--ros-args', '-p', 'config_file:=/home/developer/ros2_ws/src/robo_warehouse/config/ros2gz_bridge_config.yaml'],
         output='screen'
     )
+<<<<<<< HEAD
 
     return LaunchDescription([
         gz_sim,
         ros2gz_bridge
+=======
+    
+
+    return LaunchDescription([
+        gz_sim,
+        ros2gz_bridge,
+        tugbot_controller,
+        DeclareLaunchArgument(
+            'urdf_file',
+            default_value='/home/developer/ros2_ws/src/robo_warehouse/models/Tugbot/model.urdf'
+,
+            description='Full path to the URDF file to load'
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            arguments=[LaunchConfiguration('urdf_file')],
+            output='screen'
+        )
+>>>>>>> 495caef (slam ready)
     ])
