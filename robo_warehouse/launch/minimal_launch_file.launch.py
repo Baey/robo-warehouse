c4afb813cc6dcef6f8cf9e78e4707e52d51ec2e8
Błażej Szargut <blazejszargut@gmail.com>
2025-01-13 10:47:48 +0000

    feat: Add models directory to package setup and include nav2 as a dependency

--- robo_warehouse/launch/minimal_launch_file.launch.py
+++ robo_warehouse/launch/minimal_launch_file.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node  # Correct import for Node
+from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.actions import DeclareLaunchArgument
 from launch_ros.substitutions import FindPackageShare
